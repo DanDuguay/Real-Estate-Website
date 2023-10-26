c91996646b9df11e1136dfb44ac185b3f1a2a0f1
Dan Duguay <Duguay9@gmail.com>
2023-10-26 00:07:35 -0400

    login page pull request ready. Front end only.

--- Real_Estate_Website/client/src/App.jsx
+++ Real_Estate_Website/client/src/App.jsx
@@ -1,13 +1,17 @@
 import { Hero } from "./components/Hero/Hero";
 import { Routes, Route } from "react-router-dom";
 import UserProfile from "./components/UserProfile/UserProfile";
+import UserLogin from "./components/UserLogin/UserLogin.jsx";
+import UserRegister from "./components/UserLogin/UserRegister.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route path="/loginuser" element={<UserProfile />} />
+        <Route path="/loginuser" element={<UserLogin />} />
+        <Route path="/registeruser" element={<UserRegister />} />
+        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </>
   );
