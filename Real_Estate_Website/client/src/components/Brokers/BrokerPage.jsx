906355004345bc1a525d06d2c266a52c545acf02
Omar Ziad <omar.ziad1@outlook.com>
2023-11-06 01:31:04 -0500

    Fix nesting bug (body tag within div)

--- Real_Estate_Website/client/src/components/Brokers/BrokerPage.jsx
+++ Real_Estate_Website/client/src/components/Brokers/BrokerPage.jsx
@@ -1,28 +1,38 @@
-import {Link} from "react-router-dom";
-import "./Broker_CSS.css"
+import { Link } from "react-router-dom";
+import "./Broker_CSS.css";
 import Header from "../Header/Header.jsx";
 
 const BrokerPage = () => {
-    return (
-        <div style={{background:"var(--blue)"}}>
-            <Header/>
-        <body className="broker-page-body">
-            <section className="broker-page-section">
-                <h1>What would you like to do?</h1>
-                <br />
-                <p className="broker-page-p">
-                    <Link to="/brokercreate" className="broker-page-link">Add Broker</Link>
-                    <br />
-                    <Link to="/brokerread" className="broker-page-link">See Brokers</Link>
-                    <br />
-                    <Link to="/brokerupdate" className="broker-page-link">Update Broker</Link>
-                    <br />
-                    <Link to='/brokerdelete' className="broker-page-link">Delete Broker</Link>
-                </p>
-            </section>
-        </body>
-            </div>
-    )
-}
+  return (
+    <>
+      <div style={{ background: "var(--blue)" }}>
+        <Header />
+        <div className="broker-page-body">
+          <section className="broker-page-section">
+            <h1>What would you like to do?</h1>
+            <br />
+            <p className="broker-page-p">
+              <Link to="/brokercreate" className="broker-page-link">
+                Add Broker
+              </Link>
+              <br />
+              <Link to="/brokerread" className="broker-page-link">
+                See Brokers
+              </Link>
+              <br />
+              <Link to="/brokerupdate" className="broker-page-link">
+                Update Broker
+              </Link>
+              <br />
+              <Link to="/brokerdelete" className="broker-page-link">
+                Delete Broker
+              </Link>
+            </p>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default BrokerPage
+export default BrokerPage;
