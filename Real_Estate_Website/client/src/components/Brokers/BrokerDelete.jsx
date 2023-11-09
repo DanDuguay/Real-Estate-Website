7ba5ad0a56d3551f3befd30c37b98bea96b53157
Omar Ziad <omar.ziad1@outlook.com>
2023-11-09 18:12:32 -0500

    Create broker delete page

---
+++ Real_Estate_Website/client/src/components/Brokers/BrokerDelete.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getBroker, deleteBroker } from "../../utils/api";
+import "./BrokerDelete.css";
+
+const BrokerDelete = () => {
+  const { brokerId } = useParams();
+  const navigate = useNavigate();
+
+  const [brokerData, setBrokerData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
+
+  useEffect(() => {
+    const fetchBrokerData = async () => {
+      try {
+        const data = await getBroker(brokerId);
+        setBrokerData(data);
+      } catch (error) {
+        console.error("Error fetching Broker:", error);
+      }
+    };
+
+    fetchBrokerData();
+  }, [brokerId]);
+
+  const handleDelete = async () => {
+    try {
+      await deleteBroker(brokerId);
+      navigate("/brokerread"); // Redirect to brokerread after deletion
+    } catch (error) {
+      console.error("Error deleting Broker:", error);
+      // Handle error and show an error message if necessary
+    }
+  };
+
+  const handleCancel = () => {
+    navigate("/brokerread"); // Redirect to brokerread if the user cancels
+  };
+
+  return (
+    <div className="broker-container">
+      <h1>Delete Broker</h1>
+      <p>Are you sure you want to delete the following broker?</p>
+      <div>
+        <label>Name: {brokerData.name}</label>
+        <label>Email: {brokerData.email}</label>
+        <label>Phone Number: {brokerData.phoneNumber}</label>
+        <label>Address: {brokerData.address}</label>
+      </div>
+      <div>
+        <button type="button" onClick={handleDelete}>
+          Yes, Delete Broker
+        </button>
+        <button type="button" onClick={handleCancel}>
+          No, Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BrokerDelete;
