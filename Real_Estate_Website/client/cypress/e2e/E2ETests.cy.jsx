46f97da0b8923940bf28e8c4bf65a66e5e7f9963
danDuguay <Duguay9@gmail.com>
2023-11-10 02:17:34 -0500

    Created Some E2E tests using cypress

--- Real_Estate_Website/client/cypress/e2e/E2ETests.cy.jsx
+++ Real_Estate_Website/client/cypress/e2e/E2ETests.cy.jsx
@@ -38,7 +38,13 @@ describe("addProperties", () => {
 describe("CreateUser", () => {
   it("should be able to create a new user", () => {
     cy.visit("http://localhost:5173/user/loginuser/")
-    cy.get()
+    cy.contains("span","Sign Up").click()
+    cy.location("pathname").should('eq', "/user/registeruser")
+    cy.get("#username").type("Cypress").should("have.value", "Cypress")
+    cy.get("#email").type("Cypress@email.com").should("have.value", "Cypress@email.com")
+    cy.get("#password").type("!Q2w3e4r").should("have.value", "!Q2w3e4r")
+    cy.get("#confirm_pwd").type("!Q2w3e4r").should("have.value", "!Q2w3e4r")
+    cy.get(".user-login-button").click()
 
   })
 })
